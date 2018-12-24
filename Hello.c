6c5cf7781a25b1853a32a75739b8e4ddbfb5887f
frezzerovshik <43182862+frezzerovshik@users.noreply.github.com>
2018-12-24 21:36:54 +0300

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -4,11 +4,11 @@
 
 int main()
 {
-	static char text[1000000];
-	char c;
+	static char text[1000000];//Массив для посимвольного сохранения текста
+	char c;//Буфферный символ
 	int i = 0;
-	int end, start;
-	int counter = 0;
+	int end, start;//начало и конец предложения
+	int counter = 0;//Счетчик символов
 	FILE *fpin;
 	FILE *fpout;
 	fpin = fopen("input.txt", "rt");
@@ -22,7 +22,7 @@ int main()
 		printf("Error: output.txt\n");
 		return 1;
 	}
-	while (!feof(fpin))
+	while (!feof(fpin))//Считываем файл посимвольно
 	{
 		c = fgetc(fpin);
 		if (c == '\n');
@@ -30,7 +30,7 @@ int main()
 		{
 			text[i] = c;
 			i++;
-			if (c == '.')
+			if (c == '.' || c == '!' || c == '?')
 				text[i++] = '\0';
 		}
 	}
@@ -39,7 +39,7 @@ int main()
 	end = 0;
 	for (i = 0; i <= counter; i++)
 	{
-		if (text[i] == '.' || text[i] == '!' || text[i] == '?' || text[i] == '\0')
+		if (text[i] == '.' || text[i] == '!' || text[i] == '?' || text[i] == '\0')//Ищем конец предложения
 		{
 			int k = i;
 			do
@@ -56,14 +56,14 @@ int main()
 				char *tmp;
 				char *buf;
 				int k;
-				tmp = (char*)malloc((end - start) * sizeof(char));
+				tmp = (char*)malloc((end - start) * sizeof(char));//Динамически выделяем память под строку с предложением
 				buf = tmp;
-				for (k = start; k <= end; k++)
+				for (k = start; k <= end; k++)//Записываем предложение в динамическую строку
 				{
 					*tmp = text[k];
 					++tmp;
 				}
-				fputs(buf, fpout);
+				fputs(buf, fpout);//Запись в файл
 				fputc('\n', fpout);
 				start = end;
 				end = 0;
