4d96e3f832be2677f9ec7015dc4f44e4e3420716
frezzerovshik <43182862+frezzerovshik@users.noreply.github.com>
2018-12-25 19:23:37 +0300

    Update Hello (1).c

--- Hello (1).c
+++ Hello (1).c
@@ -1,16 +1,17 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-	static char text[1000000];
-	char c;
-	int i = 0;
-	int end, start;
-	int counter = 0;
+	static char text[1000000];// Массив для хранения текста
+	char c;//Буфферный символ
+	int i = 0;//Индекс
+	int end, start;//Начало и конец предложения
+	int counter = 0;//Счетчик символов в тексте
 	FILE *fpin;
 	FILE *fpout;
+	int num = 1;//Нумерация предложений
 	fpin = fopen("input.txt", "rt");
 	if (fpin == NULL)
 	{
@@ -22,7 +23,7 @@ int main()
 		printf("Error: output.txt\n");
 		return 1;
 	}
-	while (!feof(fpin))
+	while (!feof(fpin))//Чтение из файла
 	{
 		c = fgetc(fpin);
 		if (c == '\n');
@@ -30,47 +31,54 @@ int main()
 		{
 			text[i] = c;
 			i++;
-			if (c == '.' || c=='!' || c == '?')
-				text[i++] = '\0';
+			if (c == '.' || c == '!' || c == '?')
+				text[i++] = '\0';//Инициализация концов строк
 		}
 	}
+	//Инициализация переменных
 	counter = i;
 	start = 0;
 	end = 0;
-	for (i = 0; i <= counter; i++)
+	for (i = 0; i <= counter; i++)//Анализ последовательности символов
 	{
-		if (text[i] == '.' || text[i] == '!' || text[i] == '?' || text[i] == '\0')
+		if (text[i] == '.' || text[i] == '!' || text[i] == '?' || text[i] == '\0')//Встречен признак конца предложения
 		{
 			int k = i;
 			do
 			{
 				end = k;
 				k++;
-			} while (text[k] == '.' || text[k] == '!' || text[k] == '?' || text[k] == ' ' || text[k] == '\0');
+			} while (text[k] == '.' || text[k] == '!' || text[k] == '?' || text[k] == ' ' || text[k] == '\0');//Ищем последний разделитель
 		}
 		else
 		{
-			if (end != 0)
+			if (end != 0)//Конец предложения найден
 			{
 
 				char *tmp;
 				char *buf;
 				int k;
-				tmp = (char*)malloc((end - start) * sizeof(char));
+				tmp = (char*)malloc((end - start) * sizeof(char));//Динамически выделяем память для хранения строки с предложением
 				buf = tmp;
 				for (k = start; k <= end; k++)
 				{
+					if (k == start && text[k] == ' ')//Удаление пробела между предложениями
+					{
+						continue;
+					}
 					*tmp = text[k];
 					++tmp;
 				}
+				//Оформление записи в выходной файл
+				fprintf(fpout,"%d. " , num);
 				fputs(buf, fpout);
 				fputc('\n', fpout);
 				start = end;
 				end = 0;
+				num++;
 			}
 			else continue;
 		}
-
 	}
 	fclose(fpin);
 	fclose(fpout);
